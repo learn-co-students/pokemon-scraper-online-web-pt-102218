36695607969fbc822acbe6deca1bc12a69ae71fd
Paul Ashour <paul.ashour@gmail.com>
2019-01-20 20:03:25 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,12 +1,13 @@
 class Pokemon
   
-  attr_accessor :id, :name, :type, :db
+  attr_accessor :id, :name, :type, :db, :hp
   
-  def initialize(id:, name:, type:, db:)
+  def initialize(id:, name:, type:, db:, hp: nil)
     self.id = id
     self.name = name
     self.type = type
     self.db = db
+    self.hp = hp
   end
   
   def self.save(name, type, db)
@@ -18,17 +19,25 @@ class Pokemon
   
   def self.find(id, db)
     ins = db.prepare("SELECT * FROM pokemon WHERE id = ?;")
-    found_mon = ins.execute!(id)
+    found_mon = ins.execute!(id)[0]
+    # is there hp in the database? If not, default to 60 hp
+    # found_mon[3] ? hp = found_mon[3] : hp = 60
     if found_mon
+      # binding.pry
       self.new(
         id: id,
         name: found_mon[1],
         type: found_mon[2],
+        hp: found_mon[3],
         db: db
         )
     end
     # binding.pry
   end
   
+  def alter_hp(new_hp, db)
+    upd = db.prepare("UPDATE pokemon SET hp = ? WHERE id = ?")
+    upd.execute(new_hp, self.id)
+  end
   
 end
