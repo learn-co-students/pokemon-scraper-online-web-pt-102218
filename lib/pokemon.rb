89f7030372d213e8cbb08d46b68d8303b7d1078a
Paul Ashour <paul.ashour@gmail.com>
2019-01-20 19:42:38 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,26 @@
 class Pokemon
+  
+  attr_accessor :id, :name, :type, :db
+  
+  def initialize(id:, name:, type:, db:)
+    self.id = id
+    self.name = name
+    self.type = type
+    self.db = db
+  end
+  
+  def self.save(name, type, db)
+    ins = db.prepare("INSERT INTO pokemon(name, type) VALUES (?, ?);")
+    ins.execute(name, type)
+    id = db.execute("SELECT max(id) FROM pokemon;")
+    self.new(id: id, name: name, type: type, db: db)
+  end
+  
+  def self.find(id, db)
+    ins = db.prepare("SELECT * FROM pokemon WHERE id = ?;")
+    found_mon = ins.execute!(id)
+    # binding.prylea
+  end
+  
+  
 end
